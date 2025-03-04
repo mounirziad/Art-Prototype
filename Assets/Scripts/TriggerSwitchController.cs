f075b1e365ae7c4ca26ba1d3680cb366d1a06ad9
mounirziad <mo697861@ucf.edu>
2025-03-04 18:18:41 -0500

    even MORE controler support

--- Assets/Scripts/TriggerSwitchController.cs
+++ Assets/Scripts/TriggerSwitchController.cs
@@ -1,7 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; // Required for Input System
 using UnityEngine.SceneManagement;
+
 public class TriggerSwitchController : MonoBehaviour
 {
     [SerializeField] private Animator mySwitch = null;
@@ -14,12 +15,27 @@ public class TriggerSwitchController : MonoBehaviour
     [SerializeField] private Light light2;
     [SerializeField] private Light light3;
     [SerializeField] private Light light4;
-    //add 3 ceiling fans
+
+    // Add three ceiling fans
     [SerializeField] private CeilingFanRotation ceilingFan;
     [SerializeField] private CeilingFanRotation ceilingFan2;
     [SerializeField] private CeilingFanRotation ceilingFan3;
 
     private bool playerInsideTrigger = false;
+
+    // Input System
+    private Player playerInput;
+    private InputAction interactAction;
+
+    private void Awake()
+    {
+        // Initialize the Input System
+        playerInput = new Player();
+        playerInput.Enable();
+
+        // Get the Interact action
+        interactAction = playerInput.PlayerControls.Interact;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -37,36 +53,38 @@ public class TriggerSwitchController : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        // Check if the Interact action was triggered (E on keyboard or X on controller)
+        if (playerInsideTrigger && interactAction.triggered)
+        {
+            StartCoroutine(OpenAndCloseSwitch());
+        }
+    }
+
     private IEnumerator OpenAndCloseSwitch()
     {
         if (OnTrigger)
         {
             mySwitch.Play("LightSwitchDown", 0, 0.0f);
-            
+
+            // Turn on lights
             if (light1 != null) light1.intensity = 1f;
             if (light2 != null) light2.intensity = 1f;
             if (light3 != null) light3.intensity = 1f;
             if (light4 != null) light4.intensity = 1f;
-            if (ceilingFan != null)
-            {
-                ceilingFan.StartRotation();
-            }
 
-            if (ceilingFan2 != null)
-            {
-                ceilingFan2.StartRotation();
-            }
+            // Start ceiling fans
+            if (ceilingFan != null) ceilingFan.StartRotation();
+            if (ceilingFan2 != null) ceilingFan2.StartRotation();
+            if (ceilingFan3 != null) ceilingFan3.StartRotation();
 
-            if (ceilingFan3 != null)
-            {
-                ceilingFan3.StartRotation();
-            }
             yield return new WaitForSeconds(2f);
+
+            // Load the win screen
             SceneManager.LoadScene("Win Screen");
+
             gameObject.SetActive(false);
-
-
-
         }
         else if (OffTrigger)
         {
@@ -75,11 +93,9 @@ public class TriggerSwitchController : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void OnDestroy()
     {
-        if (playerInsideTrigger && Input.GetKeyDown(KeyCode.E))
-        {
-            StartCoroutine(OpenAndCloseSwitch());
-        }
+        // Disable the Input System when the object is destroyed
+        playerInput.Disable();
     }
 }
