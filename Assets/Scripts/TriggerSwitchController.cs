9306a54e33af907dcd1a3013d0783c5e16e2b4a5
mounirziad <mo697861@ucf.edu>
2025-03-03 23:23:28 -0500

    win screen func

--- Assets/Scripts/TriggerSwitchController.cs
+++ Assets/Scripts/TriggerSwitchController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class TriggerSwitchController : MonoBehaviour
 {
     [SerializeField] private Animator mySwitch = null;
@@ -37,39 +37,49 @@ public class TriggerSwitchController : MonoBehaviour
         }
     }
 
+    private IEnumerator OpenAndCloseSwitch()
+    {
+        if (OnTrigger)
+        {
+            mySwitch.Play("LightSwitchDown", 0, 0.0f);
+            
+            if (light1 != null) light1.intensity = 1f;
+            if (light2 != null) light2.intensity = 1f;
+            if (light3 != null) light3.intensity = 1f;
+            if (light4 != null) light4.intensity = 1f;
+            if (ceilingFan != null)
+            {
+                ceilingFan.StartRotation();
+            }
+
+            if (ceilingFan2 != null)
+            {
+                ceilingFan2.StartRotation();
+            }
+
+            if (ceilingFan3 != null)
+            {
+                ceilingFan3.StartRotation();
+            }
+            yield return new WaitForSeconds(2f);
+            SceneManager.LoadScene("Win Screen");
+            gameObject.SetActive(false);
+
+
+
+        }
+        else if (OffTrigger)
+        {
+            mySwitch.Play("LightSwitchUp", 0, 0.0f);
+            gameObject.SetActive(false);
+        }
+    }
+
     private void Update()
     {
         if (playerInsideTrigger && Input.GetKeyDown(KeyCode.E))
         {
-            if (OnTrigger)
-            {
-                mySwitch.Play("LightSwitchDown", 0, 0.0f);
-                gameObject.SetActive(false);
-                if (light1 != null) light1.intensity = 1f;
-                if (light2 != null) light2.intensity = 1f;
-                if (light3 != null) light3.intensity = 1f;
-                if (light4 != null) light4.intensity = 1f;
-                if (ceilingFan != null)
-                {
-                    ceilingFan.StartRotation();
-                }
-
-                if (ceilingFan2 != null)
-                {
-                    ceilingFan2.StartRotation();
-                }
-
-                if (ceilingFan3 != null)
-                {
-                    ceilingFan3.StartRotation();
-                }
-
-            }
-            else if (OffTrigger)
-            {
-                mySwitch.Play("LightSwitchUp", 0, 0.0f);
-                gameObject.SetActive(false);
-            }
+            StartCoroutine(OpenAndCloseSwitch());
         }
     }
 }
